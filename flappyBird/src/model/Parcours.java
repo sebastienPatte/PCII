2bbff7d7bebd156afb0bec827ddccb06650df207
sebastienPatte <sebnb91@gmail.com>
2020-01-22 17:35:48 +0100

    getParcours ne renvoie que les points visibles

--- flappyBird/src/model/Parcours.java
+++ flappyBird/src/model/Parcours.java
@@ -7,29 +7,49 @@ import java.util.concurrent.ThreadLocalRandom;
 import view.Affichage;
 
 public class Parcours {
-	
 	private ArrayList<Point> points;
-	
+	private int position;
+	/**
+	 * Constructeur
+	 */
 	public Parcours() {
-		int x = 50;
 		this.points = new ArrayList<Point>();
-		while(x <= Affichage.LARG) {
-			
-			int y = ThreadLocalRandom.current().nextInt(30, Affichage.HAUT -30 + 1);
+		for(int i=0; i < Affichage.LARG; i+=50) {
+			//On prend x entre i et i+50
+			int x = randint(i,i+50);
+			int y = randint(0, Affichage.HAUT);
 			Point p = new Point(x,y);
 			this.points.add(p);
-			x += ThreadLocalRandom.current().nextInt(0, Affichage.LARG/5 + 1);
 		}
+	}
+	
+	/** randint
+	 * @param int min
+	 * @param int max
+	 * @return random int between min and max
+	 */
+	private int randint(int min, int max) {
+		return ThreadLocalRandom.current().nextInt(min, max + 1);
 	}
 	
 	public Point[] getParcours() {
 		Point[] res = new Point[this.points.size()];
 		int i=0;
 		for (Point point : points) {
-			res[i] = point;
-			i++;
+			if(point.x - this.position <= Affichage.LARG ) {
+				res[i] = point;
+				i++;
+			}
 		}
 		return res;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
 	}
 	
 }

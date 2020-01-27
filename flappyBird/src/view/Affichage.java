fcfeceb05d84c08eb7f27c30cc94f3266419e121
root <root@s-VirtualBox>
2020-01-27 14:57:44 +0100

    méthodes affiche Ovale, Ligne et Score

--- flappyBird/src/view/Affichage.java
+++ flappyBird/src/view/Affichage.java
@@ -20,10 +20,13 @@ public class Affichage extends JPanel {
         public static final int LARG = 600;
         /** hauteur du JPanel*/
         public static final int HAUT = 400;
+        
         /** hauteur de l'ovale*/
         public static final int ovalHeight = 100;
         /** largeur de l'ovale*/
         public static final int ovalWidth = 30;
+        /** décalage de l'ovale par rapport au bord gauche de la fenêtre*/
+        public static final int ovalDec = 10;
         
         /** attribut de type {@link Etat}*/
         public Etat etat;
@@ -38,7 +41,30 @@ public class Affichage extends JPanel {
                 this.setPreferredSize(new Dimension(LARG, HAUT));
                 this.etat = etat;
         }
-
+        /** affiche l'ovale au niveau de la hauteur définie par le Modèle {@link Etat}*/
+        private void afficheOvale(Graphics g) {
+        	g.drawOval(ovalDec,this.etat.getHauteur(),ovalWidth,ovalHeight);
+        }
+        
+        /** affiche la ligne brisée à partir des points définis par la classe {@link Parcours}*/
+        private void afficheLigne(Graphics g) {
+        	Point[] p = this.etat.getParcours().getParcours();
+        	
+        	int i=1;
+        	while(i < p.length) {
+        		g.drawLine(p[i-1].x, p[i-1].y, p[i].x, p[i].y);
+        		i++;
+        	}
+        }
+        
+        /** affiche le score actuel, c'est à dire la position définie par la classe {@link Parcours}*/
+        private void afficheScore(Graphics g) {
+        	int score = this.etat.getParcours().getPosition();
+        	String strScore ="Score : "+score;
+        	FontMetrics fm = getFontMetrics(getFont());
+        	int printedLength = fm.stringWidth(strScore) +10; // on ajoute 10 pour pas etre collé au bord
+        	g.drawString(strScore, LARG-printedLength, 20);
+        }
         
         /**
          * nettoye l'affichage puis affiche l'ovale à l'endroit défini par l'{@link Etat}
@@ -48,24 +74,9 @@ public class Affichage extends JPanel {
         	//nettoie le JFrame 
         	g.clearRect(0, 0, LARG, HAUT);
         	
-        	//affiche l'ovale au niveau de la hauteur définie par le Model etat
-        	g.drawOval(10,this.etat.getHauteur(),ovalWidth,ovalHeight);
+        	this.afficheOvale(g);
+        	this.afficheLigne(g);
+        	this.afficheScore(g);
         	
-        	//TODO : sous-mécthodes pour affichage de l'ovale et de la ligne brisée
-        	
-        	Point[] p = this.etat.getParcours().getParcours();
-        	
-        	int i=1;
-        	while(i < p.length) {
-        		g.drawLine(p[i-1].x, p[i-1].y, p[i].x, p[i].y);
-        		i++;
-        	}
-        	
-        	//affichage score 
-        	int score = this.etat.getParcours().getPosition();
-        	String strScore ="Score : "+score;
-        	FontMetrics fm = getFontMetrics(getFont());
-        	int printedLength = fm.stringWidth(strScore) +10; // on ajoute 10 pour pas etre collé au bord
-        	g.drawString(strScore, LARG-printedLength, 20);
         }
 }
